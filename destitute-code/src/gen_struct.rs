823dd4211a900836fe7aac9e72443a54c3b39f98
Haf <haf@protonmail.ch>
2024-11-09 11:56:33 +0100

    refactor: cleaning up with helpers

---
+++ destitute-code/src/gen_struct.rs
@@ -0,0 +1,69 @@
+use crate::input::InputStruct;
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+
+pub fn generate_destitute_struct(input: &InputStruct) -> TokenStream {
+    let fields = input.fields.named.iter().map(|field| {
+        find_destitute_attr(field.attrs.iter())
+            .map(|_| {
+                let ty = &field.ty;
+                let mut optional_field = field.clone();
+                optional_field
+                    .attrs
+                    .retain(|attr| !attr.path().is_ident("destitute"));
+                optional_field.ty = syn::parse_quote!(Option<#ty>);
+                optional_field.into_token_stream()
+            })
+            .unwrap_or(field.to_token_stream())
+    });
+
+    let destitute_name = quote::format_ident!("Destitute{}", input.ident);
+    let vis = &input.vis;
+
+    // TODO keep attributes
+    // TODO fully qualify option?
+    // TODO do not nest options
+    quote! {
+        #vis struct #destitute_name {
+            #(#fields,)*
+        }
+    }
+}
+
+fn find_destitute_attr<'a>(
+    mut iter: impl Iterator<Item = &'a syn::Attribute>,
+) -> Option<&'a syn::Attribute> {
+    iter.find(|attr| attr.path().is_ident("destitute"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correctly_generates_struct() {
+        // ARRANGE
+        let input = InputStruct {
+            ident: syn::Ident::new("Example", proc_macro2::Span::call_site()),
+            vis: syn::Visibility::Public(Default::default()),
+            fields: syn::parse_quote!({
+                #[destitute]
+                field1: u8,
+                field2: u8
+            }),
+        };
+
+        let expected = quote! {
+            pub struct DestituteExample {
+                field1: Option<u8>,
+                field2: u8,
+            }
+        };
+
+        // ACT
+        let destitute_struct = generate_destitute_struct(&input);
+
+        // ASSERT
+        assert_eq!(destitute_struct.to_string(), expected.to_string());
+    }
+}
--- destitute-code/src/lib.rs
+++ destitute-code/src/lib.rs
@@ -1,7 +1,7 @@
+mod gen_struct;
 mod input;
 
 use proc_macro2::TokenStream;
-use quote::quote;
 
 pub fn derive_destitute(item: TokenStream) -> TokenStream {
     let input = match syn::parse2::<input::InputStruct>(item) {
@@ -9,44 +9,5 @@ pub fn derive_destitute(item: TokenStream) -> TokenStream {
         Err(err) => return err.to_compile_error(),
     };
 
-    let vis = &input.vis;
-    let fields = input.fields.named.iter().map(MaybeDestituteField::from);
-    let destitute_name = quote::format_ident!("Destitute{}", input.ident);
-
-    quote! {
-        #vis struct #destitute_name {
-            #(#fields,)*
-        }
-    }
-}
-
-struct MaybeDestituteField<'a>(&'a syn::Field);
-
-impl<'a> From<&'a syn::Field> for MaybeDestituteField<'a> {
-    fn from(field: &'a syn::Field) -> Self {
-        Self(field)
-    }
-}
-
-impl<'a> quote::ToTokens for MaybeDestituteField<'a> {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let destitute_attr = self
-            .0
-            .attrs
-            .iter()
-            .find(|attr| attr.path().is_ident("destitute"));
-
-        if let Some(_destitute_attr) = destitute_attr {
-            let vis = &self.0.vis;
-            let name = &self.0.ident;
-            let ty = &self.0.ty;
-            // TODO: gotta remove the destitute attr to not recursively add it
-            // let attrs = &self.0.attrs;
-            // quote!(#(#attrs)* #vis #name: Option<#ty>).to_tokens(tokens);
-            quote!( #vis #name: Option<#ty>).to_tokens(tokens);
-        } else {
-            let field = &self.0;
-            quote!(#field).to_tokens(tokens)
-        }
-    }
+    gen_struct::generate_destitute_struct(&input)
 }
