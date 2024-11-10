3d25b53ca288c0c5f7e357840255a295ae4c09a9
Haf <haf@protonmail.ch>
2024-11-10 17:08:52 +0100

    feat: not nesting options

--- destitute-code/src/gen_struct.rs
+++ destitute-code/src/gen_struct.rs
@@ -1,11 +1,15 @@
 use crate::input::InputStruct;
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
+use syn::spanned::Spanned;
 
 pub fn generate_destitute_struct(input: &InputStruct) -> TokenStream {
     let fields = input.fields.named.iter().map(|field| {
         if let Some(field_destitution) = FieldDestitution::find_in(&field.attrs) {
-            to_destitute_field(field, &field_destitution).to_token_stream()
+            match to_destitute_field(field, &field_destitution) {
+                Ok(field) => field.to_token_stream(),
+                Err(err) => err.to_compile_error().into_token_stream(),
+            }
         } else {
             field.to_token_stream()
         }
@@ -16,7 +20,6 @@ pub fn generate_destitute_struct(input: &InputStruct) -> TokenStream {
     let attrs = &input.attrs;
 
     // TODO fully qualify option?
-    // TODO do not nest options
     quote! {
         #(#attrs)*
         #vis struct #destitute_name {
@@ -25,18 +28,45 @@ pub fn generate_destitute_struct(input: &InputStruct) -> TokenStream {
     }
 }
 
-fn to_destitute_field(field: &syn::Field, _destitution: &FieldDestitution) -> syn::Field {
-    let ty = &field.ty;
+fn to_destitute_field(
+    field: &syn::Field,
+    _destitution: &FieldDestitution,
+) -> syn::Result<syn::Field> {
+    let ty = match &field.ty {
+        syn::Type::Path(ref ty) => ty,
+        _ => {
+            return Err(syn::Error::new(
+                field.ty.span(),
+                "#[destitute] only works on path types like `String` or `Vec<String>`",
+            ))
+        }
+    };
 
     let mut destitute_field = field.clone();
+
+    // remove the destitute attribute in the output
     destitute_field
         .attrs
         .retain(|attr| !attr.path().is_ident("destitute"));
-    destitute_field.ty = syn::parse_quote!(Option<#ty>);
-    destitute_field
+
+    // make it optional
+    destitute_field.ty = if ty
+        .path
+        .segments
+        .iter()
+        .next()
+        .is_some_and(|it| it.ident == "Option")
+    {
+        syn::Type::Path(ty.clone())
+    } else {
+        syn::parse_quote!(Option<#ty>)
+    };
+
+    Ok(destitute_field)
 }
 
 /// Parsed configuration inside a `#[destitute]` attribute for a field
+#[derive(Default)]
 struct FieldDestitution {}
 
 impl FieldDestitution {
@@ -82,5 +112,21 @@ mod tests {
 
         // ASSERT
         assert_eq!(destitute_struct.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn does_not_nest_options() {
+        // ARRANGE
+        let destitution = FieldDestitution::default();
+        let field: syn::Field = syn::parse_quote!(pub field1: Option<u8>);
+
+        // ACT
+        let destitute_field = to_destitute_field(&field, &destitution).unwrap();
+
+        // ASSERT
+        assert_eq!(
+            field.to_token_stream().to_string(),
+            destitute_field.to_token_stream().to_string()
+        );
     }
 }
